5df9450c88d7a01c13cd15787f8bc5554b48a41a
Alix <s190762@uos.ac.uk>
2018-02-16 14:14:05 +0000

    Revert "Revert "upload from other repository""
    This reverts commit eb990b81370f95830ee3312d7f9ce77d40cef4da.

---
+++ Unity Project/Project/Assets/Main.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+	public Canvas theCanvas;
+	public GameObject theBackground;
+
+	void Update () {
+		transform.Translate(0f, 0.04f, 0f); //move the camera up
+		theCanvas.transform.Translate(0f, 0.04f, 0f); //move the ui up
+		theBackground.transform.Translate(0f, 0.04f, 0f);
+	}
+}
